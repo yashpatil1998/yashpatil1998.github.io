8e27f3957498f880e5bf2e10ddf4153de0f9ce75
Yash.Patil <yash.patil@msci.com>
2025-11-08 17:44:16 +0530

    Build site for deployment

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../deploy',
+    emptyOutDir: true,
+  },
   server: {
     port: 3000,
   },
